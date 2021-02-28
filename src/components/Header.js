b69ddfbdeb3dc2988d1e087aba86d5a412c86601
WilliamOtieno <jimmywilliamotieno@gmail.com>
2021-02-28 12:49:49 +0300

    tasks

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,16 @@
 import PropTypes from 'prop-types'
+import Button from './Button'
 
 const Header = ({ title }) => {
+    const onClick = () => {
+        console.log('Click')
+    }
+
     return (
-        <header>
-            <h1 style={headingStyle}>{title}</h1>
+        <header className='header'>
+            <h1>{title}</h1>
+            <Button color='green' text='Add' onClick={onClick} />
+
         </header>
     )
 }
---
+++ src/components/Tasks.js
@@ -0,0 +1,30 @@
+const tasks = [
+    {
+        id: 1,
+        text: 'Doctors Appointment',
+        day: 'Feb 5th at 2:30pm',
+        remainder: true,
+    },
+    {
+        id: 2,
+        text: 'Meeting at School',
+        day: 'Feb 6th at 1:30pm',
+        remainder: true,
+    },
+    {
+        id: 3,
+        text: 'Food Shopping',
+        day: 'Feb 5th at 2:30pm',
+        remainder: false,
+    }
+]
+
+const Tasks = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Tasks
