cab00f1619febbb0e8634cb74cd2f790c5411f1e
WilliamOtieno <jimmywilliamotieno@gmail.com>
2021-02-28 13:35:52 +0300

    changes

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,29 +1,13 @@
-const tasks = [
-    {
-        id: 1,
-        text: 'Doctors Appointment',
-        day: 'Feb 5th at 2:30pm',
-        remainder: true,
-    },
-    {
-        id: 2,
-        text: 'Meeting at School',
-        day: 'Feb 6th at 1:30pm',
-        remainder: true,
-    },
-    {
-        id: 3,
-        text: 'Food Shopping',
-        day: 'Feb 5th at 2:30pm',
-        remainder: false,
-    }
-]
+import Task from './Task'
 
-const Tasks = () => {
+const Tasks = ({ tasks }) => {
+    
     return (
-        <div>
-            
-        </div>
+        <>
+            {tasks.map((task) => (
+                <Task key={task.id} task={task} />
+            ))}
+        </>
     )
 }
 
